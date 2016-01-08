e1447bc05f4b0f20119ac55f7096140f83c424a5
jpreynat <johan.preynat@gmail.com>
2016-01-08 17:12:52 +0100

    Use lib/converter and commander

---
+++ bin/gitbook-convert.js
@@ -0,0 +1,39 @@
+var _ = require('lodash');
+var program = require('commander');
+
+var Converter = require('../lib/converter');
+
+var allowedFormats = require('../lib/converters').allowedFormats;
+var pkg = require('../package.json');
+
+program
+    .version(pkg.version)
+    .usage('[options] <file>')
+    .option('-e, --export-dir [dirname]', 'Name of the main export directory', 'export')
+    .option('-a, --assets-dir [dirname]', 'Name of the document\'s assets export directory', 'assets');
+
+program.on('--help', function() {
+    console.log('  gitbook-convert accepts the following formats:');
+    console.log('');
+    allowedFormats.forEach(function(format) {
+        console.log('    .'+format.ext+': '+format.description);
+    });
+    console.log('');
+    console.log('  After converting your document, the corresponding GitBook files will be placed in ./export/<file>/.');
+});
+
+program.parse(process.argv);
+
+// Parse and fallback to help if no args
+if(_.isEmpty(program.parse(process.argv).args) && process.argv.length === 2) {
+    program.help();
+}
+
+var opts = {
+    filename: program.args[0],
+    rootDirectory: program.exportDir,
+    assetsDirectory: program.assetsDir
+};
+
+var converter = new Converter(opts);
+converter.convert();
