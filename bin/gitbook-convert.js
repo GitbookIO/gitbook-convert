86a5a7d465e7b1486909c016a270aadbde13e0de
jpreynat <johan.preynat@gmail.com>
2016-01-27 13:10:02 +0100

    Add --document-title argument option

--- bin/gitbook-convert.js
+++ bin/gitbook-convert.js
@@ -11,6 +11,7 @@ var pkg = require('../package.json');
 program
     .version(pkg.version)
     .usage('[options] <file>')
+    .option('-t, --document-title [string]', 'Name used for the main document title', null)
     .option('-a, --assets-dir [dirname]', 'Name of the document\'s assets export directory', 'assets')
     .option('-m, --max-depth [integer]', 'Maximum title depth to use to split your original document into sub-chapters', 2)
     .option('-p, --prefix', 'Prefix filenames by an incremental counter')
@@ -36,6 +37,7 @@ if(_.isEmpty(program.parse(process.argv).args) && process.argv.length === 2) {
 var opts = {
     filename: program.args[0],
     exportDir: program.args[1] || 'export',
+    documentTitle: program.documentTitle,
     assetsDirectory: program.assetsDir,
     titleDepth: parseInt(program.maxDepth, 10),
     prefix: program.prefix,
